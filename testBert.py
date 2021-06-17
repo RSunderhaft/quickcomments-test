edec38827a41c5da0e26de0a9047ee142163c733
Gabriela Pinto <gabrielapintogp@gmail.com>
2021-06-16 17:04:48 -0700

    matrix

--- testBert.py
+++ testBert.py
@@ -1,19 +1,13 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-import random
+from sentence_transformers import SentenceTransformer
 import nltk
-import tensorflow as tf
-from tensorflow import keras
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-import math
-from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras.layers import Embedding
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-import os
-from sentence_transformers import SentenceTransformer
+from tensorflow.keras.initializers import Constant
 
 MAX_NB_WORDS = 10000
 df= pd.read_csv("1_bert_full_connected_responses.csv")
@@ -31,6 +25,8 @@ for val in cleaned_answer_text:
     cleaned_text = cleaned_text.join(filtered_sentence)
     updated_text.append(cleaned_text)
 train_answer, test_answer, training_score, test_score = train_test_split(updated_text, score, test_size=0.2)
+print(type(train_answer))
+
 vocab_size = 8000
 embedding_dimension = 100
 max_length = 100
@@ -38,24 +34,22 @@ padding_type = 'post'
 trunc_type = 'post'
 oov_tok = "<OOV>"
 MAX_NB_WORDS = 10000
-#sentence embeddings
-model = SentenceTransformer('paraphrase-distilroberta-base-v')
+#embeddings...
+# model = SentenceTransformer('average_word_embeddings_glove.6B.300d')
+# model = SentenceTransformer('paraphrase-distilroberta-base-v1')
+model = SentenceTransformer('stsb-bert-large')
 #Our sentences we like to encode
-sentences = ['This framework generates embeddings for each input sentence','Sentences are passed as a list of string.','The quick brown fox jumps over the lazy dog.'] #answer text
-print("one")
+# sentences = ['This framework generates embeddings for each input sentence','Sentences are passed as a list of string.','The quick brown fox jumps over the lazy dog.'] #answer text
+# sentences = updated_text[0:100]
+sentences = train_answer[0:200]
 #Sentences are encoded by calling model.encode()
 sentence_embeddings = model.encode(sentences)
-print("two")
-#Print the embeddings
-for sentence, embedding in zip(sentences, sentence_embeddings):
-    print("Sentence:", sentence)
-    print("Embedding:", embedding)
-    print("")
-
 callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
 model = tf.keras.Sequential([
-tf.keras.layers.Embedding(input_dim = vocab_size, output_dim = embedding_dimension,input_length=max_length, mask_zero=True, embeddings_initializer=embeddings),
+# tf.keras.layers.Embedding(input_dim = vocab_size, output_dim = embedding_dimension,input_length=max_length, mask_zero=True, embeddings_initializer= keras.initializers.Constant(sentence_embeddings)),
+tf.keras.layers.Embedding(input_dim = vocab_size, output_dim = embedding_dimension,embeddings_initializer= Constant(sentence_embeddings), trainable=False),
 tf.keras.layers.GlobalAveragePooling1D(),
+tf.keras.layers.Flatten(),
 tf.keras.layers.Dense(5,activation='softmax')
 ])
 
