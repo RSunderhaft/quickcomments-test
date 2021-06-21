4d4a3d1f82c0edfdca1336d67550249e98a24261
Gabriela Pinto <gabrielapintogp@gmail.com>
2021-06-20 17:31:05 -0700

    model w/ sbert

--- testBert.py
+++ testBert.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sentence_transformers import SentenceTransformer
 import nltk
 from nltk.corpus import stopwords
@@ -8,8 +9,9 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.layers import Embedding
 from tensorflow.keras.initializers import Constant
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 
-MAX_NB_WORDS = 10000
 df= pd.read_csv("1_bert_full_connected_responses.csv")
 #removed nan values
 df.dropna(subset = ["cleaned_answer_text"],inplace = True)
@@ -25,35 +27,40 @@ for val in cleaned_answer_text:
     cleaned_text = cleaned_text.join(filtered_sentence)
     updated_text.append(cleaned_text)
 train_answer, test_answer, training_score, test_score = train_test_split(updated_text, score, test_size=0.2)
-print(type(train_answer))
-
-vocab_size = 8000
-embedding_dimension = 100
+vocab_size = 113289
+embedding_dimension = 1024
 max_length = 100
 padding_type = 'post'
 trunc_type = 'post'
 oov_tok = "<OOV>"
-MAX_NB_WORDS = 10000
+
+tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(train_answer)
+
+sequences = tokenizer.texts_to_sequences(train_answer)
+data = pad_sequences(sequences, maxlen = max_length, padding = padding_type, truncating=trunc_type) #look at padding with a word over null value
+
+testing_sequences= tokenizer.texts_to_sequences(test_answer)
+testing_padded=pad_sequences(testing_sequences,maxlen=max_length,padding=padding_type,truncating=trunc_type)
+
 #embeddings...
-# model = SentenceTransformer('average_word_embeddings_glove.6B.300d')
 # model = SentenceTransformer('paraphrase-distilroberta-base-v1')
 model = SentenceTransformer('stsb-bert-large')
-#Our sentences we like to encode
-# sentences = ['This framework generates embeddings for each input sentence','Sentences are passed as a list of string.','The quick brown fox jumps over the lazy dog.'] #answer text
-# sentences = updated_text[0:100]
-sentences = train_answer[0:200]
-#Sentences are encoded by calling model.encode()
+sentences = train_answer
 sentence_embeddings = model.encode(sentences)
 callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
 model = tf.keras.Sequential([
 # tf.keras.layers.Embedding(input_dim = vocab_size, output_dim = embedding_dimension,input_length=max_length, mask_zero=True, embeddings_initializer= keras.initializers.Constant(sentence_embeddings)),
-tf.keras.layers.Embedding(input_dim = vocab_size, output_dim = embedding_dimension,embeddings_initializer= Constant(sentence_embeddings), trainable=False),
+tf.keras.layers.Embedding(input_dim = vocab_size, output_dim = embedding_dimension, input_length=max_length, weights=[sentence_embeddings],trainable=False),
 tf.keras.layers.GlobalAveragePooling1D(),
 tf.keras.layers.Flatten(),
-tf.keras.layers.Dense(5,activation='softmax')
+#kernel_regularizer='l2'
+tf.keras.layers.Dense(5,activation='softmax'),
 ])
 
 #adamax, nadam, adam
-model.compile(loss='SparseCategoricalCrossentropy',optimizer='adamax', metrics=['accuracy'])
+opt = keras.optimizers.Adamax(learning_rate = 0.01)
+model.compile(loss='SparseCategoricalCrossentropy',optimizer=opt, metrics=['accuracy'])
 model.summary()
+
 model.fit(np.array(data), np.array(training_score),batch_size=128,epochs=50,validation_data=(np.array(testing_padded),np.array(test_score)), callbacks=[callback])
